ffec6466fc304a3f7f6d5e379f493e6005b3da55
Rohit <rt30082004@gmail.com>
2024-07-16 00:31:51 +0530

    Newproject

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-screen h-screen bg-red-500'>
+      <h1 className='bg-blue-500'>HEllo world</h1>
+    </div>
+  )
+}
+
+export default App
